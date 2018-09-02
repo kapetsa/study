d7e9a893eaa458c489aba2360f871051c086b015
kapetsa <42819215+kapetsa@users.noreply.github.com>
2018-09-02 16:51:32 +0200

    Add files via upload

---
+++ Ex3.java
@@ -0,0 +1,20 @@
+﻿import java.util.Scanner;
+public class Ex3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	      System.out.println("Введите длину отверстия:");
+	      double dl = sc.nextInt();
+	      System.out.println("Введите ширину отверстия:");
+	      double sh = sc.nextInt();
+	      System.out.println("Введите радиус картонки:");
+	      double rad = sc.nextInt();
+	      sc.close();
+	      double diag = Math.sqrt(Math.pow(dl, 2) + Math.pow(sh, 2));
+	      if(rad >= diag) 
+	      System.out.println("Картонка закроет отверстие");
+	      else System.out.println("Картонка не закроет отверстие");
+
+	}
+
+}
---
+++ Ex6.java
@@ -0,0 +1,36 @@
+﻿import java.util.Scanner;
+public class Ex6 {
+
+	public static void main(String[] args) {
+		
+		boolean success = false;        
+
+	    do
+	    {
+	        System.out.println("Введите четырехзначное число");
+	        Scanner sc = new Scanner(System.in);
+	        int x = sc.nextInt();
+	        
+	        
+	        if((x/10000) == 0 & (x/1000) >= 1)
+	        	success = true;
+	        
+	        if ( success == true ) {
+	        	sc.close();
+	        	int a = x%10;
+	            x = (x - a)/10;
+	            int b = x%10;
+	            x = (x - b)/10;
+	            int c = x%10;
+	            x = (x - c)/10;
+	            int d = x%10;
+	            if(a != b && a != c && a != d && b != c && b != d && c != d)
+	            System.out.println("Все цифры числа различны");
+	            else System.out.println("Цифры в числе повторяются");
+	        } else 
+	            System.out.println("Введенное Вами число не является четырехзначным!");
+	        
+	    } while ( !success);
+	}        
+		
+	}
